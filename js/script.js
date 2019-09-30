a5e944ca2d914a21a9b982e9f22c1c9c2e3f67bc
Simara Conceição <contato@simaraconceicao.com>
2019-09-30 16:49:42 -0300

    adiciona instruções no js

---
+++ js/script.js
@@ -0,0 +1,30 @@
+//captura inputTarefa
+//pega valor do input
+//criar div vazia
+//ouvir evento click no botao add
+    //valor do input 
+    //criar x 
+    //ouvir x 
+    //dar o check
+
+//criar laço para iterar
+//criar li
+    //criar span 
+        //inserir texto que vai ser o valor do input
+    //criar span
+        //inserir texto que vai ser x
+    //add classe que terá o display flex e divisor 
+
+
+
+
+
+
+//mostrar tarefinha 
+ //inserir append child li para ul
+ //inserir append child x para ul
+    
+
+
+
+
