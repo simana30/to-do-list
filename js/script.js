411c76a68cd78224470b1ed1711982d7985da6c8
Simara Conceição <contato@simaraconceicao.com>
2019-10-01 13:16:11 -0300

    adiciona classes que serão manipuladas no JS e Eventos submit, x e marcar todos

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const listaContainer = document.getElementById('listaContainer')
 const form = document.getElementById('form')
 const inputTarefa = document.getElementById('inputTarefa')
 const botaoAdd = document.getElementById('botaoAdd')
-const ul = document.getElementById('tarefas')
+const tarefas = document.getElementById('tarefas')
 const botaoMarcar = document.getElementById('botaoMarcar')
 const botaoExcluir = document.getElementById('botaoExcluir')
 
@@ -12,19 +12,61 @@ const botaoExcluir = document.getElementById('botaoExcluir')
 form.addEventListener('submit', function(evento){
     //tira o default de form 
     evento.preventDefault()
-     // criar uma li
-    const valorDigitado =  document.createElement('li')
-    //capturar valor do input da tarefa
+    //criar div e atribuir classe
+    const tarefinha = document.createElement('div')
+    tarefinha.className = "tarefinha"
+    tarefas.appendChild(tarefinha)
+
+    //criar p que vai ter conteudo do valor digitado
+    const valorDigitado = document.createElement('p')
+    valorDigitado.className = "tarefa-digitada"
     valorDigitado.textContent = inputTarefa.value
-    // inserir input tarefa dentro do li
+    
 
-    console.log(valorDigitado)
- })
+    //criar p que vai ter x como conteúdo
+    const paragrafoX = document.createElement('p')
+    paragrafoX.textContent = "x"
+    
+    //estrututura de identação
+    tarefinha.appendChild(valorDigitado)
+    tarefinha.appendChild(paragrafoX)
+    
+    //ouvir o parágrafo x  e adicionar a classe que risca a tarefa e faz o inverso (toggle)
+    paragrafoX.addEventListener('click', function(evento){
+        if(valorDigitado.classList.contains('tarefa-digitada')){
+            valorDigitado.classList.remove('tarefa-digitada')
+            valorDigitado.classList.add('tarefa-executada')
+        } else {
+            valorDigitado.classList.add('tarefa-digitada')
+            valorDigitado.classList.remove('tarefa-executada')
+        }
+       
+    })
+})
+
+
+//ouvir o botaoMarcar tudo iterar todas as tarefas e add a classe que risca tudo
+botaoMarcar.addEventListener('click', function(evento){
+    for(let i = 1; i < tarefas.lentgh; i++) {
+        if(valorDigitado.classList.contains('tarefa-digitada')){
+            valorDigitado.classList.remove('tarefa-digitada')
+            valorDigitado.classList.add('tarefa-executada')
+        } else {
+            valorDigitado.classList.add('tarefa-digitada')
+            valorDigitado.classList.remove('tarefa-executada')
+        }
+    }
+})
 
 
 
 
 
 
+
+
+
+//ouvir o botaoExcluir, iterar e criar uma nodelist vazia, excluir todos os itens ou resetar 
+//form.reset();
 
 
